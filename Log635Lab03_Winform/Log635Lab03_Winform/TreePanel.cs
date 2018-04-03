995826f0f287a67391624e65f5989f8ad307bc19
Jean-Rene Choiniere <jeanrenechoiniere@gmail.com>
2018-04-03 16:04:30 -0400

    changed graphique rendering

--- Log635Lab03_Winform/Log635Lab03_Winform/TreePanel.cs
+++ Log635Lab03_Winform/Log635Lab03_Winform/TreePanel.cs
@@ -13,10 +13,15 @@ namespace Log635Lab03_Winform
     public partial class TreePanel : Panel
     {
         private TreeNode _root;
+        private int _depth = 0;
 
         public TreeNode Root
         {
-            set { _root = value; Invalidate(); }
+            set
+            {
+                _root = value;
+                Invalidate();
+            }
         }
 
         public TreePanel()
@@ -38,32 +43,41 @@ namespace Log635Lab03_Winform
             var graphics = e.Graphics;
 
             if (_root != null)
-                PaintTreeNode(graphics, _root, 0, false, new PointF(10000, 50));
+                PaintTreeNode(graphics, _root, 0, false, new PointF(50, 10000));
         }
 
-        private void PaintTreeNode(Graphics graphics, TreeNode node, int depth, bool right, PointF ParentPosition)
+        private void PaintTreeNode(Graphics graphics, TreeNode node, int depth, bool right, PointF parentPosition)
         {
             if (node == null)
             {
                 return;
             }
             
-            var multiple = right ? 1 : -1;
+            var ymultiple = right ? 1 : -0.5f;
+            var xmultiple = right ? 1 : .5f;
 
-            var x = ParentPosition.X + 100 * multiple * (30 / (depth + 1));
-            var y = ParentPosition.Y + 100;
+
+            int childCount = 0;
+            GetNeededSpace(node, ref childCount);
+
+            var x = parentPosition.X + 500 + (childCount * 0.5f) * xmultiple + depth * 2;
+            var y = parentPosition.Y + (150 + childCount * 20f) * ymultiple + depth * 2;
             var loc = new PointF(x, y);
             
-            graphics.DrawString(node.Column, new Font("Consolas", 10), new SolidBrush(Color.Black), loc);
+            graphics.DrawString(node.TrueFilterExpression, new Font("Consolas", 10), new SolidBrush(Color.Black), loc);
 
             if (depth != 0)
             {
-                graphics.DrawLine(new Pen(Color.Black), new PointF(ParentPosition.X, ParentPosition.Y + 15),
-                    new PointF(loc.X, loc.Y - 15));
+                var yadd = right ? 15: 2;
+                graphics.DrawLine(
+                    new Pen(Color.Black),
+                    new PointF(parentPosition.X + 35, parentPosition.Y + yadd),
+                    new PointF(loc.X - 5, loc.Y + 3)
+                );
             }
 
-            var px = x - (ParentPosition.X - x) / 2;
-            var py = y - (ParentPosition.Y - y) / 2;
+            var px = x - (parentPosition.X - x) / 2;
+            var py = y - (parentPosition.Y - y) / 2;
 
             //if (right)
             //    graphics.DrawString(node.TrueFilterExpression, new Font("Consolas", 6),
@@ -72,8 +86,23 @@ namespace Log635Lab03_Winform
             //    graphics.DrawString(node.FalseFilterExpression, new Font("Consolas", 6),
             //        new SolidBrush(Color.Black), new PointF(px, py));
 
-            PaintTreeNode(graphics, node.ChildTrue, depth + 1, true, loc);
-            PaintTreeNode(graphics, node.ChildFalse, depth + 1, false, loc);
+            PaintTreeNode(graphics, node.ChildTrue, 3, true, loc);
+            PaintTreeNode(graphics, node.ChildFalse, 5, false, loc);
+        }
+
+        private void GetNeededSpace(TreeNode node, ref int nodeCount)
+        {
+            if (node.ChildTrue != null)
+            {
+                nodeCount++;
+                GetNeededSpace(node.ChildTrue, ref nodeCount);
+            }
+
+            if (node.ChildFalse != null)
+            {
+                nodeCount++;
+                GetNeededSpace(node.ChildFalse, ref nodeCount);
+            }
         }
     }
 }
